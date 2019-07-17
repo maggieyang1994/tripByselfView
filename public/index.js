55c08fd65093ff09ab1d7b4db5779e7325388d0b
Maggie.yang <Maggie.Yang@royaleinternational.com>
2019-07-17 11:17:29 +0800

    test serviceWorker

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 if(navigator.serviceWorker){
   navigator.serviceWorker.register("./serviceWorker.js").then(function(res){
     console.log("service worker 注册成功", res)
+  }).catch(e => {
+    console.log(e.message)
   })
 }
+console.log(rinima)
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,53 @@
+// The files we want to cache
+let cacheName = 'rinima';
+// 缓存文件和请求路径
+let cacheFile = [
+    '/index.html',
+    '/manifest.json',
+    '/trip/getTrips',
+    'https://triprecord-server.herokuapp.com/trip/getTrips'
+
+]
+
+
 self.addEventListener("install", function(e){
     self.skipWaiting()
-    console.log('install', e)
+    // 确保安装成功后 添加文件到缓存
+    e.waitUtil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(cacheFile)
+        })
+    )
+
+});
+
+self.addEventListener("fetch", function(e){
+    console.log("捕捉到fetch请求")
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            // 如果缓存有 直接返回缓存 否则就存到缓存中
+
+            // 缓存请求路径
+            let cloneRequest = e.request.clone()
+            if(response) {
+                console.log("没有缓存  直接请求")
+                return response
+            }
+            else {
+                console.log("读取缓存")
+               return  fetch(cloneRequest).then(response => {
+                    let cloneResponse = response.clone()
+                    console.log(response.type)
+                    if(!response || response.stauts !== 200){
+                        return response
+                    }else{
+                        caches.open(cacheName).then(cache => {
+                            cache.put(e.request, cloneResponse)
+                        })
+                    }
+                    return response
+                })
+            }
+        })
+    )
 })
